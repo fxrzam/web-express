80d714e4013fb76a53a07840859408d8b0fa5a59
Farzam Fatah <fxrzam@gmail.com>
2026-02-07 23:02:47 +0300

    refactor: restructure routes and middleware for improved organization

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.use(()=>{
-    console.log('we got a new request')
-})
-
 app.listen(3000, () =>{
     console.log('listening on port 3000')
 })
+
+
+app.get('/r/:subreddit' ,(req, res)=>{
+    const {subreddit} = req.params;
+    res.send(`<h1> browsing the ${subreddit} subreddit`)
+})
+
+app.get('/r/:subreddit/:postID' ,(req, res)=>{
+    const {subreddit, postID} = req.params;
+    res.send(`<h1>veiwing the post ID:${postID} in the subreddit of ${subreddit}</h1>`)
+})
+
+app.get('/cats' ,(req, res)=>{
+    res.send('meow')
+})
+app.get('/dogs' ,(req, res)=>{
+    res.send('woof')
+})
+app.get('/' ,(req, res)=>{
+    res.send('this is the home page')
+})
+
+app.use((req, res)=>{
+    res.send('<h1>this is my web page</h1>')
+})
+
+
+
+
